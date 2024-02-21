0c2db18138ca1963f0b613f9e3c34e869b3f206b
administrator <administrator@administrators-MacBook-Pro-6.local>
2024-02-21 08:26:55 +0700

    fabric with commited dist

---
+++ dist/fabric/CellContainerNativeComponent.d.ts
@@ -0,0 +1,8 @@
+import type { Int32 } from "react-native/Libraries/Types/CodegenTypes";
+import type { ViewProps } from "react-native";
+interface NativeProps extends ViewProps {
+    index?: Int32;
+}
+declare const _default: import("react-native/Libraries/Utilities/codegenNativeComponent").NativeComponentType<NativeProps>;
+export default _default;
+//# sourceMappingURL=CellContainerNativeComponent.d.ts.map
